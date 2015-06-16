b90107df514379618203ecb831435e3faea84aa2
randbw <apbward@gmail.com>
2015-06-16 16:56:03 +1000

    table is displaying through airplane. route fixed for airplane model

--- app/assets/javascripts/backbone/views/flightView.js
+++ app/assets/javascripts/backbone/views/flightView.js
@@ -15,29 +15,37 @@ app.FlightView = Backbone.View.extend({
   },
 
   addTable: function () {
-    var airplane = new app.Airplane({id: this.model.get('airplane_id')});
-    console.log(this.model.get('airplane_id'));
-    var rows = airplane.row;
-    var columns = airplane.columns;
-    var $seats = $('#seats');
+    var airplane = new app.Airplane({ id: this.model.get('airplane_id') });
+    console.log( this.model.get('airplane_id') );
+
+    airplane.fetch().done( function () {
+      console.log(airplane)
+
+      var rows = airplane.get("rows");
+      var columns = airplane.get("columns");
+      var $seats = $('#seats');
+        
+      var table = document.createElement('TABLE');
+      table.border='1';
       
-    var table = document.createElement('TABLE');
-    table.border='1';
-    
-    var tableBody = document.createElement('TBODY');
-    table.appendChild(tableBody);
-      
-    for (var i = 0; i < rows; i++){
-       var tr = document.createElement('TR');
-       tableBody.appendChild(tr);
-       
-       for (var j = 0; j < columns; j++){
-           var td = document.createElement('TD');
-           td.width='75';
-           td.appendChild(document.createTextNode("Cell " + i + "," + j));
-           tr.appendChild(td);
-       }
-    }
-    $seats.append(table);    
-}
+      var tableBody = document.createElement('TBODY');
+      table.appendChild(tableBody);
+        
+      for (var i = 0; i < rows; i++){
+         var tr = document.createElement('TR');
+         tableBody.appendChild(tr);
+         
+         for (var j = 0; j < columns; j++){
+             var td = document.createElement('TD');
+             td.width='75';
+             td.appendChild(document.createTextNode("Cell " + i + "," + j));
+             tr.appendChild(td);
+         }
+      }
+      $seats.append(table); 
+
+
+
+    });   
+  }
 });
